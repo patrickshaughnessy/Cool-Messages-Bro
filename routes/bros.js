68370f0dde331aca85814de52cb8293fe1ec720b
Patrick Shaughnessy <pshaughn86@gmail.com>
2015-12-01 14:19:52 -0800

    heroku deploy

--- routes/bros.js
+++ routes/bros.js
@@ -3,7 +3,7 @@
 var express = require('express');
 var router = express.Router();
 
-var ensureAuthenticated = require('../config/ensureAuthenticated')
+var ensureAuthenticated = require('../config/ensureAuthenticated');
 var User = require('../models/user');
 
 router.get('/', ensureAuthenticated, function(req, res){
